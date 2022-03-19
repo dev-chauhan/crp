d4543214d407ef7ae352c2b31e8850900c8e5864
Dev Chauhan <devgiri@iitk.ac.in>
2022-03-19 16:41:02 +0530

    Correct headers

--- src/crp.c
+++ src/crp.c
@@ -1,4 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
