32a8ca3bb38c0b3a9e1b90244f75f7df51477e21
Dev Chauhan <devgiri@iitk.ac.in>
2022-03-19 16:39:41 +0530

    Add cmd code

---
+++ src/crp.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc != 4)
+    {
+        printf("Usage: %s <command> <pid> <name>\n", argv[0]);
+        return -1;
+    }
+    char *command = argv[1];
+    int pid = atoi(argv[2]);
+    char *name = argv[3];
+
+    int fd;
+    if((fd = open("/dev/crp")) < 0)
+    {
+        printf("Error: open /dev/crp failed\n");
+        return -1;
+    }
+    if(strcmp(command, "checkpoint") == 0)
+    {
+        unsigned long args[2] = {0, pid};
+        if(read(fd, args, sizeof(args)) < 0)
+        {
+            printf("Error: read /dev/crp failed\n");
+            return -1;
+        }
+    }else if(strcmp(command, "restore") == 0)
+    {
+        unsigned long args[2] = {1, pid};
+        if(write(fd, args, sizeof(args)) < 0)
+        {
+            printf("Error: write /dev/crp failed\n");
+            return -1;
+        }
+    }else{
+        printf("Error: command %s is not supported\n", command);
+        return -1;
+    }
+    return 0;
+}
